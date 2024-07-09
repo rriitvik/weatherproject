3248141511b3ba29a099a01b7342db63edd8fc72
Ritvik <gopalkaritvik117@gmail.com>
2024-07-09 15:15:21 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactRefresh()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'], // Externalizing React and ReactDOM
+    },
+  },
+});
