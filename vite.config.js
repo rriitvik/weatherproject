d76114f25b400a52e460be64b7116bd5227d5bc8
Ritvik <gopalkaritvik117@gmail.com>
2024-07-09 15:21:20 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
+// vite.config.js
 
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom'], // Externalizing React and ReactDOM
+      external: ['@mui/icons-material'],
     },
   },
 });
